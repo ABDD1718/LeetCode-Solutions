93811a85c2eee09751d45419a43a921bf1a5e192
ABDD1718 <lakshyashukla03@gmail.com>
2026-09-26 02:20:49 +0530

    Add Easy C++ Solution for 1. Two Sum from LeetCode

---
+++ LeetCode/C++/Easy/1. Two Sum/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            int num=nums[i];
+            int moreneeded=target-num;
+            if(mpp.find(moreneeded)!=mpp.end()){
+                return {mpp[moreneeded],i};
+            }
+            mpp[num]=i;
+        }
+        return {-1,-1};
+
+           }
+};
