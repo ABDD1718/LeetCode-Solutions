33b478e95a7a12aafa82615c2dedae56b56b155f
ABDD1718 <lakshyashukla03@gmail.com>
2026-09-26 16:32:17 +0530

    Add Easy C++ Solution for 1539. Kth Missing Positive Number from LeetCode

---
+++ LeetCode/C++/Easy/1539. Kth Missing Positive Number/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low=0;
+        int high=arr.size();
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]-1-mid<k) low=mid+1;
+            else high=mid;
+        }
+        return low+k;
+    }
+};
