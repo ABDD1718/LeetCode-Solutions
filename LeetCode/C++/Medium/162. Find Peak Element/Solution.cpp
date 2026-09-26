68fa14db8cc9e4672aedf308250c111d09496f6a
ABDD1718 <lakshyashukla03@gmail.com>
2026-09-26 23:47:10 +0530

    Add Medium C++ Solution for 162. Find Peak Element from LeetCode

--- LeetCode/C++/Medium/162. Find Peak Element/Solution.cpp
+++ LeetCode/C++/Medium/162. Find Peak Element/Solution.cpp
@@ -3,7 +3,7 @@ public:
     int findPeakElement(vector<int>& nums) {
         int low=1;
         int high=nums.size()-2;
-        if(nums[0]>nums[1]) return 0;
+        if((nums.size()==1)|| nums[0]>nums[1]) return 0;
         if(nums[high+1]>nums[high]) return high+1;
         while(low<=high){
             int mid=low+(high-low)/2;
